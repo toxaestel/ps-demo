5280c8b5cb79bc04eccf2ee25f2329a7a8688118
akhomenko <anton.khomenko@gettyimages.com>
2019-10-16 22:42:59 +0300

    validation

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+import { UserSettings } from "../data/user-settings";
+import { NgForm, NgModel } from "@angular/forms";
+
+@Component({
+  selector: "app-user-settings-form",
+  templateUrl: "./user-settings-form.component.html",
+  styleUrls: ["./user-settings-form.component.css"]
+})
+export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+}
